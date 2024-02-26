e91937436bfab2941ce9f55a3b24947d3ad7c61e
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2024-02-26 12:23:11 +0300

    test

--- android/src/main/java/com/unact/yandexmapkit/YandexMapController.java
+++ android/src/main/java/com/unact/yandexmapkit/YandexMapController.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
